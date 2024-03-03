d08ec3833f117502a5e1a696df2ed33c31eee13d
KaushalSethia <kksroxx95@gmail.com>
2024-03-03 14:32:48 +0530

    code

--- Book.java
+++ Book.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Book {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public double getPrice() {
+        return price;
+    }
+    @Override
+    public String toString() {
+        return "Title: " + title + "\nAuthor: " + author + "\nPrice: $" + price + "\n";
+    }
 }
